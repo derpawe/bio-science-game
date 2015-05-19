418efa806968a08be2e0238c59e841e35f862e5f
Patrick Weiner <patrick.weiner@pawe.at>
2015-05-19 22:02:21 +0200

    Enemy Patrolling added

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
+
+    public float chaseSpeed = 5f;
+    public float patrolSpeed = 3.5f;
+    public Transform[] patrolPoints;
 
     private NavMeshAgent _navAgent;
     private GameObject _player;
+
+    private int currentPatrolPoint;
+    private bool isChasing = false;
 
     void Awake()
     {
@@ -12,17 +20,54 @@ public class EnemyController : MonoBehaviour {
         _player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void OnTriggerStay(Collider collider)
+    void Update()
     {
-        if (collider.gameObject == _player)
+        if (isChasing)
         {
             CatchPlayer();
         }
+        else
+        {
+            Patrol();
+        }
     }
 
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject == _player)
+        {
+            isChasing = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        _navAgent.Stop();
+        isChasing = false;
+        _navAgent.SetDestination(patrolPoints[currentPatrolPoint].position);
+        _navAgent.Resume();
+    }
+
+    private void Patrol()
+    {
+        _navAgent.speed = patrolSpeed;
+        if (_navAgent.remainingDistance == 0)
+        {
+            if (currentPatrolPoint == patrolPoints.Length - 1)
+            {
+                currentPatrolPoint = 0;
+            }
+            else
+            {
+                currentPatrolPoint++;
+            }
+            _navAgent.SetDestination(patrolPoints[currentPatrolPoint].position);
+        }
+    }
 
     private void CatchPlayer()
     {
+        _navAgent.speed = chaseSpeed;
         _navAgent.SetDestination(_player.transform.position);
     }
 }
