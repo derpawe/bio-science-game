6971e3593895a48b22a2ed0dec25bd4eb5514a47
Patrick Weiner <patrick.weiner@pawe.at>
2015-05-27 18:31:43 +0200

    first level

---
+++ Assets/Scripts/3D/DoorTriggerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTriggerController : MonoBehaviour {
+
+    private GameObject _player;
+
+    void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.2f);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == _player)
+        {
+            Debug.Log("LOAD PUZZLE");
+            int i = Application.loadedLevel;
+            Application.LoadLevel(i + 1);
+        }
+    }
+}
