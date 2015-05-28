c77726a1ad5d91d1f7febb9adf6b4e584b019aa1
Patrick Weiner <patrick.weiner@pawe.at>
2015-05-28 21:13:52 +0200

    added splashscreen changed menu

---
+++ Assets/LoadNextScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextScene : MonoBehaviour {
+
+    public void LoadNextLevel()
+    {
+        int i = Application.loadedLevel;
+        Application.LoadLevel(i + 1);
+    }
+}
